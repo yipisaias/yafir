ec65db901af0931c939a23af5acff5d05237bb62
yipisaias <yipisaias@gmail.com>
2024-10-28 20:37:09 -0300

    add open graph metas

--- api/index.php
+++ api/index.php
@@ -7,10 +7,17 @@
     ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Yafir</title>
-    <link rel="icon" href="<?= $iconUrl; ?>">
+
+    <title>Yafir - Yet Another Favicon Relayer</title>
+
+    <link rel="canonical" href="https://yafir.vercel.app">
+    <link rel="icon" href="<?= $iconUrl ?>">
+    <meta property="og:site_name" content="Yafir">
+    <meta property="og:title" content="Yafir - Yet Another Favicon Relayer">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://yafir.vercel.app">
 </head>
 <body style="margin: 0px; height: 100%; display: flex; justify-content: center; align-items: center;">
-    <img src="<?= $iconUrl; ?>" alt="loading..." style="margin: auto;">
+    <img src="<?= $iconUrl ?>" alt="loading..." style="margin: auto;">
 </body>
 </html>
